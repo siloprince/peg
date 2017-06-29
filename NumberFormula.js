b49bb2e37ae0349b3c2ee5ab0c910b91826630a6
Yoshiaki Araki <yoshiaki.araki@g-pc-4601.intra.gree-office.net>
2017-06-29 09:33:41 +0900

    Done unsigned

--- NumberFormula.js
+++ NumberFormula.js
@@ -6,7 +6,7 @@ let parser = peg.generate(pegStr);
 
 //let parser = peg.generate("start = (' '/'a' / 'b')+");
 //console.log(parser.parse('a'));
-console.log(parser.parse('-.20 * (3 + 4)/(1+1)'));
+console.log(parser.parse('-1.2'));
 
 function getPegStr() {
   return `
@@ -22,6 +22,12 @@ NumberFormula
         if (element[1] === "-") { return result - element[3]; }
       }, head);
     }
+/ tail:(_ ("+" / "-") _ Term)* {
+      return tail.reduce(function(result, element) {
+        if (element[1] === "+") { return result + element[3]; }
+        if (element[1] === "-") { return result - element[3]; }
+      }, 0);
+    }
 
 Term
 = head:Factor tail:(_ ("*" / "/") _ Factor)* {
@@ -34,39 +40,28 @@ Term
 Factor
 = "(" _ expr:NumberFormula _ ")" { return expr; }
   / UnsignedNumber
+  / Sequence
+
+Sequence 
+= _ [^+-a-z \\t\\n\\r]+
+{
+  return 100;
+}
 
 UnsignedNumber
-= $(UnsignedInt / UnsignedFloat)
+= $( UnsignedFloat / UnsignedInt)
 {
   return parseFloat(text());
 }
 
 UnsignedFloat
-= UnsignedInt '.' Digit*
-/ '.' Digit+
-/ Signed '.' Digit+
+= UnsignedInt '.' [0-9]*
+/ '.' [0-9]+
 
 
 UnsignedInt
-= Zero
-/ NonZeroInt
-
-NonZeroInt
-= Digit19 Digit*
-/ Signed Digit19 Digit*
-
-Zero
 = '0'
-/ Signed '0'
-
-Signed
-= [\+\-]
-
-Digit19
-= [1-9]
-
-Digit
-= [0-9]
+/ [1-9] [0-9]*
 
 _ "wsp"
   = [ \\t\\n\\r]*
